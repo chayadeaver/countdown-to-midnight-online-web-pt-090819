c85ed21b1f7d9c29de0d263aaead9bd45bb4ab3c
Chaya Deaver <chayadeaver@gmail.com>
2019-09-11 15:15:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  sleep(5)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
