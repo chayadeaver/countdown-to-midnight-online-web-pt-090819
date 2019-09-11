0324b5fd7122954c6fdae58f7df23ef3d85d96d9
Chaya Deaver <chayadeaver@gmail.com>
2019-09-11 14:50:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,8 @@
 #write your code here
+
 def countdown(number)
   while number > 0 do
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
-  "HAPPY NEW YEAR!"
 end
-
-
-# #def countdown_with_sleep(number)
-#
-# end
