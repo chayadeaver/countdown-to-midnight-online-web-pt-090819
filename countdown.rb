f4d0e372042211a882c508f3a9c9bec1ae158f43
Chaya Deaver <chayadeaver@gmail.com>
2019-09-11 15:06:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(number)
-  number = 10
   while number > 0 do
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  puts 
   "HAPPY NEW YEAR!"
 end
